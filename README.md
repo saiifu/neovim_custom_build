100a1f06dd8c2169b830f2418f8f2aef6f59e127
shobu13 <saiifulshobuj22@gmail.com>
2025-08-27 23:23:36 +0600

    inital updated

---
+++ README.md
@@ -0,0 +1,11 @@
+## Install Neovim
+### Fedora
+``` bash
+sudo dnf install nvim 
+```
+### Debian 
+``` bash
+sudo apt install nvim
+```
+
+
---
+++ init.lua
@@ -0,0 +1,12 @@
+## Install NeoVim
+### For Fedora
+``` bash
+sudo dnf install nvim 
+```
+### For Debian based
+``` bash
+sudo apt install nvim
+```
+
+
+
