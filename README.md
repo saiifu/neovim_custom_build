a2eda4e814c8461e970c2cd2135875847f7f800f
shobu13 <saiifulshobuj22@gmail.com>
2025-08-28 23:07:40 +0600

    First Developed

--- README.md
+++ README.md
@@ -1,49 +1,138 @@
-## Install Neovim
-### Fedora
+# Install Custom Neovim
+Things i've used there :
+- nvim 
+- nnn 
+- packer 
+- knap 
+- lsp 
+
+
+##### Install nvim on Fedora
+
 ```bash
 sudo dnf install nvim 
 ```
-### Debian 
+##### Install nvim on Debian/Ubuntu
+
 ```bash
 sudo apt install nvim
 ```
-Check the nvim version first cause it will only work on version 0.11.3+
+
+**Check the nvim version first cause it will only work on version 0.11.3+** 
+
 ```bash
 nvim --version
 ```
+---
 
-Install nvim-lspconfig 
+##### Install lspconfig via git clone 
+
 ```bash
 git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
 ```
 
-#### Then install these command one after one for python, javascript/typescript, rust, bash 
+If you want to use latex auto-completion and suggestion also, you can install latex on your machine:
+
+**For Fedora**
+
+```bash
+sudo dnf update -y
+sudo dnf install texlive-scheme-full -y
+```
+
+Check the latex version:
+
+```bash
+pdflatex --version
+```
+---
+
+##### Install Latex LSP (texlab)
+
+Run these commands one by one 
+
+```bash
+curl -LO https://github.com/latex-lsp/texlab/releases/latest/download/texlab-x86_64-linux.tar.gz
+tar -xvzf texlab-x86_64-linux.tar.gz
+sudo mv texlab /usr/local/bin/
+sudo chmod +x /usr/local/bin/texlab
+```
+
+Check texlab version:
+
+```bash
+texlab --version
+```
+Install Pandoc also. 
+
+```bash
+sudo apt install Pandoc
+sudo dnf install Pandoc
+```
+---
+
+##### Install KNAP for auto preview Markdown, HTML 
+
+Run these commands one by one .
+
+```bash
+mkdir -p ~/.local/share/nvim/site/pack/plugins/start 
+cd ~/.local/share/nvim/site/pack/plugins/start/ 
+git clone --depth 1 https://github.com/frabjous/knap.git 
+```
+---
+
+##### Install NNN for file explorer
+
+```bash
+sudo dnf install nnn 
+
+```
+**nnn icon configuration (you must have the NERD font installed on your machine)** 
+
+```bash
+cd ~
+git clone https://github.com/jarun/nnn.git
+cd nnn
+make O_NERD=1
+sudo make install
+
+```
+---
+
+**Then install these command one by one for python, javascript/typescript, rust, bash**
+
 ```bash
 sudo npm i -g pyright 
 npm install -g typescript typescript-language-server
 rustup component add rust-analyzer
 npm install -g bash-language-server
 ```
+---
 
+##### Install Packer
 
-#### Install Packer
 ```bash
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 ```
 
-Then move the init.lua file to the followin folder 
+**Then move the init.lua file to the followin folder**
+
 ```bash
 ~/.config/nvim/ 
 ```
 
-then run the command 
+Run the command :
+
 ```bash
 nvim
 ```
-and in visual mode press shift+: and type 
+and in visual mode press shift+: and type :
+
 ```bash
 PackerSync
 ```
+
 
 ## Now you can see auto completion and press ctrl+n for tree file structure
--- init.lua
+++ init.lua
@@ -1,12 +1,180 @@
-## Install NeoVim
-### For Fedora
-``` bash
-sudo dnf install nvim 
-```
-### For Debian based
-``` bash
-sudo apt install nvim
-```
 
+-- PACKER: plugin manager
+--
+require('packer').startup(function(use)
+
+  use 'wbthomason/packer.nvim'          -- Packer itself
+  use 'neovim/nvim-lspconfig'           -- LSP configs
+  use 'hrsh7th/nvim-cmp'                -- Autocomplete engine
+  use 'hrsh7th/cmp-nvim-lsp'            -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-buffer'              -- Buffer words source
+  use 'hrsh7th/cmp-path'                -- File paths
+  use 'saadparwaiz1/cmp_luasnip'        -- Snippets source
+  use 'L3MON4D3/LuaSnip'                -- Snippet engine
+  use 'windwp/nvim-autopairs'           -- auto bracket
+  use 'lervag/vimtex'                   -- Best LaTeX plugin (syntax, compile, TOC, etc.)
+  use 'ltex-plus/vscode-ltex'           -- Grammar + spellcheck (optional, for English writing)
+  use 'iurimateus/luasnip-latex-snippets.nvim'  -- Latex Snippet 
+  use 'tanvirtin/monokai.nvim'          -- Monokai theme
+  
+ 
+  use 'rafamadriz/friendly-snippets'
+  -- NNN FileExplorer
+  use {
+  "luukvbaal/nnn.nvim",
+
+  config = function()
+    require("nnn").setup({
+      explorer = {
+	icons = true,
+        width = 24,
+        side = "topleft",
+        tabs = true,
+      },
+      picker = {
+        style = {
+          width = 0.9,
+          height = 0.8,
+          xoffset = 0.5,
+          yoffset = 0.5,
+          border = "rounded",
+        },
+	icons = true,
+        tabs = true,
+        fullscreen = true,
+      },
+      auto_open = {
+        setup = nil,
+        tabpage = nil,
+        empty = false,
+        ft_ignore = { "gitcommit" },
+      },
+      windownav = { 
+        left = "<C-w>h",
+        right = "<C-w>l",
+        next = "<C-w>w",
+        prev = "<C-w>W",
+      },
+    })
+  end
+}
+
+
+end)
+
+
+-- LSP SETUP
+
+local kmap = vim.keymap.set 
+local lspconfig = require('lspconfig')
+
+-- List of LSP servers to enable
+local servers = { 'pyright', 'ts_ls', 'rust_analyzer', 'bashls', 'texlab' }  -- add more if needed
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {}
+end
+
+
+
+
+-- nvim-cmp setup 
+
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'luasnip' },
+  })
+})
+
+
+
+-- Basic autopairs  
+
+require('lspconfig').bashls.setup{}
+require('nvim-autopairs').setup{}
+require'luasnip-latex-snippets'.setup()
+require('lspconfig').texlab.setup{}
+require('monokai').setup {
+  palette = require('monokai').pro, -- or .soda, .ristretto
+}
+require("luasnip.loaders.from_vscode").lazy_load()
+
+
+
+
+
+-- Integrate autopairs with autocomplete
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+
+-- KNAP Settings 
+
+local gknapsettings = {
+    -- LaTeX settings
+    texoutputext = "pdf",
+    textopdf = "pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
+    textopdfviewerlaunch = "mupdf %outputfile%",
+    textopdfviewerrefresh = "kill -HUP %pid%",
+    -- Markdown → HTML
+    mdoutputext = "html",
+    mdtohtml = "pandoc %docroot% -s -o %outputfile%",
+    mdtohtmlviewerlaunch = "firefox %outputfile%",
+    mdtohtmlviewerrefresh = "kill -HUP %pid%",
+    -- Delay (ms) before auto-processing
+    delay = 250
+}
+
+
+-- Optional settings
+
+vim.o.completeopt = "menuone,noinsert,noselect"
+vim.cmd('filetype plugin indent on')
+vim.cmd[[colorscheme monokai]]
+
+-- Absolute line numbers
+vim.opt.number = true
+-- Optional: relative numbers
+vim.opt.relativenumber = true
+
+
+-- KeyBindings 
+
+
+kmap({ 'n', 'v', 'i' }, '<F5>', function() require("knap").process_once() end)
+-- F6: close viewer
+kmap({ 'n', 'v', 'i' }, '<F6>', function() require("knap").close_viewer() end)
+-- F7: toggle auto-preview
+kmap({ 'n', 'v', 'i' }, '<F7>', function() require("knap").toggle_autopreviewing() end)
+-- F8: forward jump (for LaTeX SyncTeX)
+kmap({ 'n', 'v', 'i' }, '<F8>', function() require("knap").forward_jump() end)
+vim.g.knap_settings = gknapsettings
+vim.g.knap_filetypes = { "markdown", "pandoc", "html", "tex" }
+
+-- NNN Explorer Mode
+vim.api.nvim_set_keymap('n', '<C-A-n>', '<cmd>NnnExplorer<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-A-n>', '<cmd>NnnExplorer<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-A-n>', '<cmd>NnnExplorer %:p:h<CR>', { noremap = true, silent = true })
+
+-- Picker Mode
+vim.api.nvim_set_keymap('n', '<C-A-p>', '<cmd>NnnPicker<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-A-p>', '<cmd>NnnPicker<CR>', { noremap = true, silent = true })
 
 
